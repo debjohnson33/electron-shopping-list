5b768ab97610edf7da261e1d573f6004088774db
debjohnson <debjohnson_11@yahoo.com>
2018-05-23 14:53:58 -0500

    Added clear items functionality

--- main.js
+++ main.js
@@ -65,7 +65,10 @@ const mainMenuTemplate = [
 				}
 			},
 			{
-				label: 'Clear Items'
+				label: 'Clear Items',
+				click(){
+					mainWindow.webContents.send('item:clear');
+				}
 			},
 			{
 				label: 'Quit',
