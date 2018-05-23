e71ad7a9c18d212563f04bcf5651979c49af7ef4
debjohnson <debjohnson_11@yahoo.com>
2018-05-23 14:46:30 -0500

    Added ipcRenderer/add item functionality to app

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow, Menu } = electron;
+const {app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 let addWindow;
@@ -46,6 +46,12 @@ function createAddWindow() {
 		addWindow = null;
 	});
 }
+// Catch item:add
+ipcMain.on('item:add', function(e, item){
+	mainWindow.webContents.send('item:add', item);
+	addWindow.close();
+})
+
 
 // Create menu Template
 const mainMenuTemplate = [
