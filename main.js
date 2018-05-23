456a9f72afd77e1337c2117270d2c7b3f1b9b517
debjohnson <debjohnson_11@yahoo.com>
2018-05-22 23:08:39 -0500

    Added custom menu options

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow } = electron;
+const {app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
@@ -16,4 +16,23 @@ app.on('ready', function(){
 		protocol: 'file:',
 		slashes: true
 	}));
+	// Build menu from template
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+	// Insert menu
+	Menu.setApplicationMenu(mainMenu);
 });
+
+
+// Create menu Template
+const mainMenuTemplate = [
+	{
+		label:'File',
+		submenu:[
+			{label: 'Add Item'},
+			{label: 'Clear Items'},
+			{label: 'Quit', click() {
+				app.quit();
+			}}
+		]
+	}
+] 
